9445284a71edfc772ffe8d0e766e8eaf1cc7f8f7
Fabien Farella <dfg>
2020-11-03 17:50:51 +0100

    Made launche args more flexible

--- jupyter_code_server_proxy/__init__.py
+++ jupyter_code_server_proxy/__init__.py
@@ -3,14 +3,38 @@ import os
 
 def setup_code_server():
     def _get_code_server_cmd(port):
-        return [
-            "code-server",
+        ENV_VARS = os.environ
+
+        launcher = ENV_VARS.get("VSCODE_PATH", "code-server")
+
+        args = [
+            launcher,
             "--bind-addr",
             f"0.0.0.0:{port}",
             "--disable-telemetry",
-            "--auth",
-            "none",
         ]
+        auth = ENV_VARS.get("VSCODE_AUTH", "none")
+        args.append("--auth")
+        args.append(auth)
+
+        cert = ENV_VARS.get("VSCODE_CERT")
+        cert_key = ENV_VARS.get("VSCODE_CERT_KEY")
+        if cert and cert_key:
+            args.append("--cert")
+            args.append(cert)
+            args.append("--cert-key")
+            args.append(cert_key)
+
+        data_dir = ENV_VARS.get("VSCODE_DATA_DIR")
+        if data_dir:
+            args.append("--user-data-dir")
+            args.append(data_dir)
+        ext_dir = ENV_VARS.get("VSCODE_EXT_DIR")
+        if ext_dir:
+            args.append("--extensions-dir")
+            args.append(ext_dir)
+
+        return args
 
     return {
         "command": _get_code_server_cmd,
